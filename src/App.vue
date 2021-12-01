84f8363129edb09b9c6e49657cc69e1ebec8dd2a
DollyChen_CYC <dolly726033@gmail.com>
2021-12-02 01:10:17 +0800

    feat: update CSS reset settings and basic style settings

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
   <div id="twitter-app">
     <Navbar />
-    <router-view />
-    <PopularUsersCard />
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
-import Navbar from "./components/Navbar.vue";
-import PopularUsersCard from "./components/PopularUsersCard.vue";
+import Navbar from "./components/Navbar.vue"
+
 export default {
   name: "App",
-  components: { Navbar, PopularUsersCard },
+  components: { Navbar },
 };
 </script>
-
-<style>
-#twitter-app {
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  margin: 0 auto;
-  height: 100%;
-  max-width: 1440px;
-}
-router-view {
-  margin: 0 20px 0 20px;
-}
-</style>
--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,57 +1,61 @@
 <template>
-  <div>
+  <div class="testPage">
     <!-- 測試頁面，確認字體有拉到 SCSS 的樣式 -->
-    <h3>Test page</h3>
-    <i class="fas fa-align-left"></i>
-    <h3>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum
-      debitis mollitia ipsam quod, provident, perferendis nisi maiores nihil
-      sapiente animi incidunt laudantium blanditiis aspernatur beatae? Obcaecati
-      quae officiis fugiat! 帳號Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Autem voluptatum debitis mollitia ipsam quod, provident,
-      perferendis nisi maiores nihil sapiente animi incidunt laudantium
-      blanditiis aspernatur beatae? Obcaecati quae officiis fugiat! 帳號Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum
-      debitis mollitia ipsam quod, provident, perferendis nisi maiores nihil
-      sapiente animi incidunt laudantium blanditiis aspernatur beatae? Obcaecati
-      quae officiis fugiat! 帳號Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Autem voluptatum debitis mollitia ipsam quod, provident,
-      perferendis nisi maiores nihil sapiente animi incidunt laudantium
-      blanditiis aspernatur beatae? Obcaecati quae officiis fugiat! 帳號Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum
-      debitis mollitia ipsam quod, provident, perferendis nisi maiores nihil
-      sapiente animi incidunt laudantium blanditiis aspernatur beatae? Obcaecati
-      quae officiis fugiat! 帳號Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Autem voluptatum debitis mollitia ipsam quod, provident,
-      perferendis nisi maiores nihil sapiente animi incidunt laudantium
-      blanditiis aspernatur beatae? Obcaecati quae officiis fugiat! 帳號Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum
-      debitis mollitia ipsam quod, provident, perferendis nisi maiores nihil
-      sapiente animi incidunt laudantium blanditiis aspernatur beatae? Obcaecati
-      quae officiis fugiat! 帳號Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Autem voluptatum debitis mollitia ipsam quod, provident,
-      perferendis nisi maiores nihil sapiente animi incidunt laudantium
-      blanditiis aspernatur beatae? Obcaecati quae officiis fugiat! 帳號Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatum
-      debitis mollitia ipsam quod, provident, perferendis nisi maiores nihil
-      sapiente animi incidunt laudantium blanditiis aspernatur beatae? Obcaecati
-      quae officiis fugiat! 帳號Lorem ipsum dolor sit amet consectetur
-      adipisicing elit. Autem voluptatum debitis mollitia ipsam quod, provident,
-      perferendis nisi maiores nihil sapiente animi incidunt laudantium
-      blanditiis aspernatur beatae? Obcaecati quae officiis fugiat! 帳號
-    </h3>
+    <section class="center-view">
+      <h3>Test page</h3>
+      <i class="fas fa-align-left"></i>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+        voluptatum debitis mollitia ipsam quod, provident, perferendis nisi
+        maiores nihil sapiente animi incidunt laudantium blanditiis aspernatur
+        beatae? Obcaecati quae officiis fugiat! 帳號Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Autem voluptatum debitis mollitia ipsam
+        quod, provident, perferendis nisi maiores nihil sapiente animi incidunt
+        laudantium blanditiis aspernatur beatae? Obcaecati quae officiis fugiat!
+        帳號Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+        voluptatum debitis mollitia ipsam quod, provident, perferendis nisi
+        maiores nihil sapiente animi incidunt laudantium blanditiis aspernatur
+        beatae? Obcaecati quae officiis fugiat! 帳號Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Autem voluptatum debitis mollitia ipsam
+        quod, provident, perferendis nisi maiores nihil sapiente animi incidunt
+        laudantium blanditiis aspernatur beatae? Obcaecati quae officiis fugiat!
+        帳號Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+        voluptatum debitis mollitia ipsam quod, provident, perferendis nisi
+        maiores nihil sapiente animi incidunt laudantium blanditiis aspernatur
+        beatae? Obcaecati quae officiis fugiat! 帳號Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Autem voluptatum debitis mollitia ipsam
+        quod, provident, perferendis nisi maiores nihil sapiente animi incidunt
+        laudantium blanditiis aspernatur beatae? Obcaecati quae officiis fugiat!
+        帳號Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+        voluptatum debitis mollitia ipsam quod, provident, perferendis nisi
+        maiores nihil sapiente animi incidunt laudantium blanditiis aspernatur
+        beatae? Obcaecati quae officiis fugiat! 帳號Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Autem voluptatum debitis mollitia ipsam
+        quod, provident, perferendis nisi maiores nihil sapiente animi incidunt
+        laudantium blanditiis aspernatur beatae? Obcaecati quae officiis fugiat!
+        帳號Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+        voluptatum debitis mollitia ipsam quod, provident, perferendis nisi
+        maiores nihil sapiente animi incidunt laudantium blanditiis aspernatur
+        beatae? Obcaecati quae officiis fugiat! 帳號Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Autem voluptatum debitis mollitia ipsam
+        quod, provident, perferendis nisi maiores nihil sapiente animi incidunt
+        laudantium blanditiis aspernatur beatae? Obcaecati quae officiis fugiat!
+        帳號
+      </h3>
+    </section>
+    <section class="right-card">
+      <PopularUsersCard />
+    </section>
   </div>
 </template>
 
 <script>
+import PopularUsersCard from "../components/PopularUsersCard.vue"
+
 export default {
   name: "test-page",
+  components: {
+    PopularUsersCard
+  }
 };
 </script>
-
-<style scoped>
-div {
-  height: 100vh;
-  overflow-y: scroll;
-}
-</style>
