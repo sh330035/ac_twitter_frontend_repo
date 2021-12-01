f9a132631c72089e1238c3f1df7e3cb0d6defc1f
david3335 <sh330035@gmail.com>
2021-12-01 13:02:22 +0800

    feat. "scss setting & navbar component"

---
+++ src/components/PopularUsersCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="popular">
+    <div class="popular_users"></div>
+  </div>
+</template>
+
+<style scoped>
+.popular {
+  height: 100vh;
+  min-width: 350px;
+  border: 1px solid red;
+}
+</style>
