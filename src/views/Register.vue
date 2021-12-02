f077515c21b47aa912549cc57e73c9123429a343
DollyChen_CYC <dolly726033@gmail.com>
2021-12-02 16:40:01 +0800

    feat: add user login function

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="register">
-    <Toast :ToastMessage="ToastMessage" />
-    <div class="logo"></div>
-    <h2>建立你的帳號</h2>
-    <account-setting-form @after-form-submit="afterFormSubmit" />
-  </div>
+  <section class="center-view">
+    <div class="register">
+      <Toast :ToastMessage="ToastMessage" />
+
+      <div class="logo"></div>
+      <h2>建立你的帳號</h2>
+      <account-setting-form @after-form-submit="afterFormSubmit" />
+      <span class="link" @click="$router.push('/login')">取消</span>
+    </div>
+  </section>
 </template>
 
 <script>
-import AccountSettingForm from "../components/AccountSettingForm.vue";
-import Toast from "../components/AlertToast.vue";
+import AccountSettingForm from "../components/AccountSettingForm.vue"
+import Toast from "../components/AlertToast.vue"
 
 export default {
   components: {
@@ -25,23 +29,25 @@ export default {
       },
       // 預設後端回傳錯誤訊息
       backendReturnStatus: false,
-    };
+    }
   },
   methods: {
     afterFormSubmit(accountDetail) {
-      console.log("註冊資料送出：", accountDetail);
+      console.log("註冊資料送出：", accountDetail)
 
       // Toast 測試 模擬Email重複
-      if (!this.backendReturnStatus) {
-        this.sendToastMessage();
+      if (this.backendReturnStatus) {
+        this.sendToastMessage()
       }
+
+      this.$router.push({ name: "login" })
     },
     // 修改 toast message 讓 toast 監看到變化，觸發視窗跳出
     sendToastMessage() {
-      this.ToastMessage.dataStatus = "";
-      this.ToastMessage.dataStatus = "error";
+      this.ToastMessage.dataStatus = ""
+      this.ToastMessage.dataStatus = "error"
 
-      this.ToastMessage.message = "Email 已重複註冊!";
+      this.ToastMessage.message = "Email 已重複註冊!"
     },
   },
 };
