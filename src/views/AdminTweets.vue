3840c5eb8db78e225d6fb6dbb54deba828a89223
DollyChen_CYC <dolly726033@gmail.com>
2021-12-02 13:14:33 +0800

    feat: update routes

---
+++ src/views/AdminTweets.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>This is Admin Tweets page</h1>
+</template>
---
+++ src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test-page">
+    <!-- 測試頁面，確認字體有拉到 SCSS 的樣式 -->
+    <section class="center-view">
+      <h1>This is User Page</h1>
+    </section>
+    <section class="right-card">
+      <PopularUsersCard />
+    </section>
+  </div>
+</template>
+
+<script>
+import PopularUsersCard from "../components/PopularUsersCard.vue"
+
+export default {
+  components: {
+    PopularUsersCard
+  },
+};
+</script>
