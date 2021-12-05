c0a4396a36e53e00b63910fa6eb1475c17b9e640
david3335 <sh330035@gmail.com>
2021-12-05 23:44:51 +0800

    feat "API basic setting & Login authorization"

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -3,7 +3,21 @@ import axios from 'axios'
 // 向後端確認後再修正
 const baseURL = 'http://localhost:3000/api'
 
-export const apiHelper = axios.create({
+const axiosInstance = axios.create({
   baseURL
 })
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // 取出 token
+    const token = localStorage.getItem('token')
+    // 如果 token 存在，則寫入 header 設定
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+  },
+  (err) => Promise.reject(err)
+)
+
+export const apiHelper = axiosInstance
