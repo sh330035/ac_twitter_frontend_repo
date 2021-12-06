bb2cf59268dc156c407a7151152c73a913902a7a
david3335 <sh330035@gmail.com>
2021-12-07 03:05:17 +0800

    feat "admin login api"

--- src/api/admin.js
+++ src/api/admin.js
@@ -5,23 +5,22 @@ import { apiHelper } from '../utils/helper'
 export default {
   // 管理員登入
   // router.post('/admin/signin', adminController.signIn)
-  adminSignIn ({ email, password}) {
-    return apiHelper.post(`/admin/signin`,{ email, password})
+  adminSignIn({ account, password }) {
+    return apiHelper.post(`/admin/signin`, { account, password })
   },
   // 取得管理員可見的所有用戶清單
   // router.get('/admin/users', authenticated, authenticatedAdmin, adminController.getUsers)
-  getAdminUsers () {
+  getAdminUsers() {
     return apiHelper.get(`/admin/users`)
   },
   // 取得管理員可見的 tweet 清單
   // router.get('/admin/tweets', authenticated, authenticatedAdmin, adminController.getTweets)
-  getAdminTweets () {
+  getAdminTweets() {
     return apiHelper.get(`admin/tweets`)
   },
   // 管理員刪除特定一筆 tweet
   // router.delete('/admin/tweets/:id', authenticated, authenticatedAdmin, adminController.deleteTweet)
-  deleteTweet ({ tweetId }) {
-    return apiHelper.delete(`/admin/tweets/${ tweetId }`)
+  deleteTweet({ tweetId }) {
+    return apiHelper.delete(`/admin/tweets/${tweetId}`)
   }
 }
-
