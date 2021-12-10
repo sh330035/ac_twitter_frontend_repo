9a18dcbdf1b6aadb42c2bb50f962db58809b0206
david3335 <sh330035@gmail.com>
2021-12-10 16:06:03 +0800

    feat. "chanllenge new router & view"

---
+++ src/views/Chatroom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="notice">
+    <section class="center-view">
+      <h1>Chatroom</h1>
+    </section>
+    <section class="right-room">
+      <h1>Chatroom</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chatroom",
+  components: {},
+};
+</script>
---
+++ src/views/Notice.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="notice">
+    <section class="center-view">
+      <PageNameBanner :banner-title="bannerTitle" />
+      <h1>Notice</h1>
+    </section>
+    <section class="right-card">
+      <PopularUsersCard />
+    </section>
+  </div>
+</template>
+
+<script>
+import PopularUsersCard from "../components/PopularUsersCard.vue";
+import PageNameBanner from "../components/PageNameBanner.vue";
+export default {
+  name: "Notice",
+  components: {
+    PopularUsersCard,
+    PageNameBanner,
+  },
+  data() {
+    return {
+      bannerTitle: "通知",
+    };
+  },
+};
+</script>
